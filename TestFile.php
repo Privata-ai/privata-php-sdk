8f5f66f3f71c57c8d3cb0967da8c88bc2cf06d6b
AndreNunes <andre-sm-nunes@hotmail.com>
2020-04-30 12:06:46 +0100

    Add Test File
    Added a test file to simulate the SDK integration with other applications. The code has also been reorganized and documented.

---
+++ TestFile.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <h1>SDK File</h1>
+    </head>
+
+    <body>
+
+    <?php
+    
+    require 'sdk.php';
+
+        //Test variables
+
+        $dbKeyuser = '11dfce72-7889-40e5-b1c8-19865c619eda';
+        $dbSecret = '05f3aed260dc756af246e166d1eef9c8f43cd61b';
+        $queryTest = '[
+            {
+              "sql": "SELECT first_name, last_name, phone_number, email FROM Patient",
+              "timestamp": 1567493198,
+              "user": "4353479",
+              "group": "Medics",
+              "returnedRows": 3
+            },
+            {
+              "sql": "SELECT blood_type, notes FROM Medical_Record",
+              "timestamp": 1567493198,
+              "user": "4353479",
+              "group": "Medics",
+              "returnedRows": 3
+            },
+            {
+              "sql": "SELECT address, vat_number, social_security_number, email FROM Patient_Receipts",
+              "timestamp": 1293234,
+              "user": "4353479",
+              "group": "Administrative",
+              "returnedRows": 1
+            }
+          ]';
+
+        //Tests in order to simulate an API call using the PHP-SDK file
+        $testObject = new PrivataAudit;
+        $resultAuthentication = $testObject -> initialize($dbKeyuser, $dbSecret);
+        $resultQuery = $testObject -> submitQuery($queryTest);
+        
+        echo "<br>";
+        print_r ("Authentication result: ".$resultAuthentication);          //should print 200 in a successful request
+        echo "<br>";
+        print_r ("Query result: ".$resultQuery);                            //should print 200 in a successful request
+
+    ?>
+
+    </body>
+
+</html>
--- sdk.php
+++ sdk.php
@@ -1,38 +1,9 @@
-<html>
-    <head>
-        <h1>SDK File</h1>
-    </head>
+<?php
 
-    <body>
-    
-    <?php
-
-        $dbKeyuser = '11dfce72-7889-40e5-b1c8-19865c619eda';
-        $dbSecret = '05f3aed260dc756af246e166d1eef9c8f43cd61b';
-        $queryTest = '[
-            {
-              "sql": "SELECT first_name, last_name, phone_number, email FROM Patient",
-              "timestamp": 1567493198,
-              "user": "4353479",
-              "group": "Medics",
-              "returnedRows": 3
-            },
-            {
-              "sql": "SELECT blood_type, notes FROM Medical_Record",
-              "timestamp": 1567493198,
-              "user": "4353479",
-              "group": "Medics",
-              "returnedRows": 3
-            },
-            {
-              "sql": "SELECT address, vat_number, social_security_number, email FROM Patient_Receipts",
-              "timestamp": 1293234,
-              "user": "4353479",
-              "group": "Administrative",
-              "returnedRows": 1
-            }
-          ]';
-
+    /** 
+     * Class with an initialize function and query submition function
+     * 
+    */
     class PrivataAudit {
 
         protected $dbKey = '';
@@ -40,10 +11,19 @@
         protected $errorMessage = '';
 
 
+        /**
+         * Initialize
+         * 
+         * Signs in the user using his dbKey and dbSecret
+         * 
+         * Fails with an error if the dbKey or dbSecret do not match
+         */
         public function initialize($dbKeyuser, $dbSecret){
-            global  $api_key, $dbKey;
+            global $dbKey;
+
             $api_key = 'AIzaSyDO-JXjcrO9x5sSX30mLQdSpu3_r7yI9gY';
             $url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='.$api_key.'';
+
             $dbKey = $dbKeyuser;
             $localdbKey =  $dbKeyuser.'@blockbird.ventures';
             $data = [
@@ -51,7 +31,7 @@
                 'password' => $dbSecret,
                 'returnSecureToken' => true
             ];
-            //echo $api_key;
+
             try{
                 $curl = curl_init($url);
                 curl_setopt($curl, CURLOPT_POST, true);
@@ -59,67 +39,77 @@
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
                 $response = curl_exec($curl);
                 curl_close($curl);
-                //echo $response;
-               
             } catch( Exception $e){
-                echo 'Caught an exception while attempting to connect to Firebase: ', $e->getMessage(), "\n";
+                return($e->getMessage());
             }
 
             if(http_response_code() == 200){
                 $obj = json_decode($response, true);                //convert json to array
-                if (isset ($obj['idToken'])){
+                if (isset ($obj['idToken'])){                       
                     global $idToken;
                     $idToken = $obj['idToken'];
-                    //echo $obj;
-                    return 200;
+                    return 200;                                     //query has been submited successfully
                 }
-                else if (isset($obj["error"]["message"])) {
+                else if (isset($obj["error"]["message"])) {         //Checks in order to detect an unsuccessful submit request
                     global $errorMessage;
                     $errorMessage = $obj["error"]["message"];
+                    return $errorMessage;
                 }
-            }else {
-                echo "There has been an error while trying to connect to Firebase \n". http_response_code();
+            } else {
+                return(http_response_code());
             }
-            echo"Authentication successful \n ";
         }
 
+
+        /**
+         * Submit Queries
+         * 
+         * Connects to the database and submits user queries in json format
+         * 
+         * Fails with an error message should a request fail.
+         */
         public function submitQuery($query){
             global $idToken, $dbKey;
+
             $apiUrl = 'https://api-sandbox.privata.ai';
             $headers = array(
                 "Content-Type: application/json",
                 "Authorization: Bearer ".$idToken
             );
             $url = $apiUrl."/databases"."/".$dbKey."/queries";
+
             try{
                 $curl = curl_init($url);
                 curl_setopt($curl, CURLOPT_POST, true);
-                curl_setopt($curl, CURLOPT_HEADER, true);
                 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
                 $response = curl_exec($curl);
-
                 curl_close($curl);
-                echo $response;
-               
             } catch( Exception $e){
-                echo 'Caught an exception while attempting to connect to Firebase: ', $e->getMessage(), "\n";
+                return($e->getMessage());
             }
 
-
+            if(http_response_code() == 200){
+                $obj = json_decode($response, true);
+                if (isset($obj["error"])) {                             //Checks in order to detect an unsuccessful submit request
+                    if(isset($obj["message"])){
+                        global $errorMessage;
+                        $errorMessage = $obj["message"];
+                        return $errorMessage;
+                    } else {
+                        global $errorMessage;
+                        $errorMessage = $obj["error"];
+                        return $errorMessage;
+                    }
+                } else{
+                    return 200;                                         //query has been submited successfully
+                }
+            }else {
+                return(http_response_code());
+            }
         }
 
     }
-        
-    $testObject = new PrivataAudit;
-    $testObject -> initialize($dbKeyuser, $dbSecret);
-    $testObject -> submitQuery($queryTest);
-
-    ?>
     
-    </body>
-
-
-
-</html>
+?>
